83000d8cfb26ff4d3e99dd00a2a605ecbd5b1167
Zaido219 <jpmalbas4@gmail.com>
2026-09-13 15:46:33 +0800

    feat: WIP check codes

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
-from builders.prompt_builder import PromptBuilder
-from services.imagen_service import ImagenClientWrapper
-
+import os
+from pathlib import Path
+from src.builders.prompt_builder import PromptBuilder
+from src.services.imagen_service import ImagenClientWrapper
+from src.services.storage_service import ImageSaver
 
 if __name__ == "__main__":
-    pb = PromptBuilder()
-    icw = ImagenClientWrapper()
+    builder = PromptBuilder()
+    image_saver = ImageSaver()
+    client = ImagenClientWrapper()
 
-    creative_brief = pb.orchestrate("Goku eating fishballs at a street food cart in Manila")
+    payload = builder.orchestrate("Goku eating fishballs at a street food cart in Manila")
+    images = client.generate_image_from_payload(payload)
+
+    project_root = Path(__file__).resolve().parent.parent
+    target_dir = project_root/ "storage"/ "approved"
+    output_filepath = target_dir / "goku_fishballs.png"
+
+    if images:
+        ImageSaver.save(images[0], str(output_filepath))
