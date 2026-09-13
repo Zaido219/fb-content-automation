c2077a6811e60989ba6b0736d4d754d4e7674d77
Zaido219 <jpmalbas4@gmail.com>
2026-09-13 13:44:00 +0800

    feat: initial brand_rules.json

---
+++ src/builders/prompt_builder.py
@@ -0,0 +1,10 @@
+class PromptBuilder:
+    def __init__(self):
+        pass
+    # helper method to read the brand rules file
+    def _get_brand_rules():
+        pass
+    # method that combines all rules for the imagen api
+    def orchestrate(prompt:str)->str:
+
+        pass
