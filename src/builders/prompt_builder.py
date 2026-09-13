142cab035c30315ac255a4fa5c884eb377311215
Zaido219 <jpmalbas4@gmail.com>
2026-09-13 13:58:40 +0800

    feat: reading brand_rules on the prompt_builder service

--- src/builders/prompt_builder.py
+++ src/builders/prompt_builder.py
@@ -1,10 +1,26 @@
-class PromptBuilder:
-    def __init__(self):
-        pass
-    # helper method to read the brand rules file
-    def _get_brand_rules():
-        pass
-    # method that combines all rules for the imagen api
-    def orchestrate(prompt:str)->str:
+import os
+import json
 
-        pass
+class PromptBuilder:
+    def __init__(self): 
+        self.base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+
+    # helper method to read the brand rules file
+    def _get_brand_rules(self):
+        file_path = os.path.join(self.base_dir, "config", "brand_rules.json")
+
+        with open(file_path, 'r') as file:
+            return json.load(file)
+
+    # method that combines all rules for the imagen api
+    def orchestrate(self)->str:
+        brand_rules = self._get_brand_rules()
+
+        print(brand_rules)
+
+
+
+if __name__ == "__main__":
+    pb = PromptBuilder()
+    pb.orchestrate()
+        
